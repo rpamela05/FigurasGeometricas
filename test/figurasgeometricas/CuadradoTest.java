4225064643a94b9797abd9675ac55ba4641f61f5
rpamela05 <ruth.carbajal@tecsup.edu.pe>
2025-01-11 15:01:32 +0100

    Creacion test de clase cuadrado

---
+++ test/figurasgeometricas/CuadradoTest.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package figurasgeometricas;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rpame
+ */
+public class CuadradoTest {
+    
+    public CuadradoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCalcularArea() {
+        System.out.println("calcularArea");
+        Cuadrado instance = new Cuadrado(4);
+        double expResult = 16;
+        double result = instance.calcularArea();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of calcularPerimetro method, of class Cuadrado.
+     */
+    @Test
+    public void testCalcularPerimetro() {
+        System.out.println("calcularPerimetro");
+        Cuadrado instance = new Cuadrado(8);
+        double expResult = 32;
+        double result = instance.calcularPerimetro();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
